19bcb6a4a01a5e01542acc63c7c244efe4102f45
mmatt625 <notmmatt@gmail.com>
2020-04-14 13:22:18 -0500

    Updated a bunch of things for GitHub

--- cogs/startup.py
+++ cogs/startup.py
@@ -8,7 +8,7 @@ class startup(commands.Cog):
 #    async def status_task(self, client, asyncio):
 #        while True:
 #           await asyncio.sleep(10)
-#           await client.change_presence(status = discord.Status.online, activity=discord.Game('Thanks for using Doob! | gitlab.com/mmatt625/doob'))
+#           await client.change_presence(status = discord.Status.online, activity=discord.Game('Thanks for using Doob! | github.com/mmatt625/doob'))
 #           await asyncio.sleep(10)
 #           await client.change_presence(status = discord.Status.online, activity=discord.Game('Running on Doob v1.06'))
 #           await asyncio.sleep(10)
@@ -16,7 +16,7 @@ class startup(commands.Cog):
     # Prints in console when the bot is online.
     @commands.Cog.listener()
     async def on_ready(self, client, status_task):
-        await client.change_presence(status = discord.Status.online, activity=discord.Game('-help for commands. | gitlab.com/mmatt625/doob'))
+        await client.change_presence(status = discord.Status.online, activity=discord.Game('-help for commands. | github.com/mmatt625/doob'))
 
 #        client.loop.create_tast(status_task()), client, status_task
         print('Doob is online!')
