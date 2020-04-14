bb9647b6ba36f43a8970a0b220e5132ee27d6ace
mmatt625 <notmmatt@gmail.com>
2020-04-14 13:33:35 -0500

    github instead of gitlab for boards

--- cogs/todo.py
+++ cogs/todo.py
@@ -6,12 +6,12 @@ class todo(commands.Cog):
     def __init__(self, client):
         self.client = client
         
-    # Gives the todo list from GitLab.
+    # Gives the todo list from GitHub.
     @commands.command(aliases=['board', 'whatsnext', 'update'])
     async def todo(self, ctx):
         embed = discord.Embed(title="Here's the link for what's up next for Doob.", description="The Todo list for Doob.", colour=discord.Color.blue())
 
-        embed.add_field(name="GitLab Issue Board", value="https://gitlab.com/mmatt625/doob/-/boards")
+        embed.add_field(name="GitHub Issue Board", value="https://github.com/mmatt625/doob/projects/1")
         embed.set_thumbnail(url=doob_logo)
         await ctx.send(embed=embed)
 
