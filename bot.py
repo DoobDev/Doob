4b82b2f4afa1845e84464cedb31bd9ed4a257d97
mmatt625 <notmmatt@gmail.com>
2020-05-05 05:49:01 -0500

    fixed status

--- bot.py
+++ bot.py
@@ -18,6 +18,11 @@ client = commands.Bot(command_prefix = get_prefix)
 
 client.remove_command("help")
 
+@client.event
+async def on_ready():
+    print('Doob is online!')
+    await client.change_presence(status = discord.Status.online, activity=discord.Game('-help for commands. | github.com/mmatt625/doob'))
+
 @client.command()
 async def load(ctx, extension):
     print(f'Loaded {extension}')
