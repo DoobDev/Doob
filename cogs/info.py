f6c658d4434eee8d4dd0434d54e11eac38817fe5
mmatt625 <notmmatt@gmail.com>
2020-05-22 22:40:01 -0500

    thing?

--- cogs/info.py
+++ cogs/info.py
@@ -15,6 +15,7 @@ class info(commands.Cog):
         embed.add_field(name="Description", value="A Discord bot made by mmatt using discord.py, he felt like making it so he did B)")
         embed.add_field(name="Developer", value="The creator of Doob is <@308000668181069824>")
         embed.add_field(name="Hosted on", value="AWS Server")
+        embed.add_field(name="Bot's Server Count", value=str(len(client.servers)))
         embed.add_field(name="The ping for Doob is...", value=f" :ping_pong: {round(self.client.latency * 1000)} ms")
         embed.add_field(name="Library", value="discord.py")
         embed.add_field(name="Top.gg Link", value="https://top.gg/bot/680606346952966177")
