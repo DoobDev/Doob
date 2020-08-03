9ba21f4028ed259605520ae42d101b008e7b8a13
mmatt625 <notmmatt@gmail.com>
2020-08-03 15:54:09 -0500

    REINSTALL PYTHON LULW

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import json
 import asyncio
 import logging
 import os
+import aiohttp
 
 from discord.ext import commands
 
@@ -21,7 +22,7 @@ client.remove_command("help")
 @client.event
 async def on_ready():
     print('Doob is online!')
-    await client.change_presence(status = discord.Status.online, activity=discord.Game('-help for commands. | github.com/mmatt625/doob'))
+    await client.change_presence(status = discord.Status.online, activity=discord.Game('-help for commands. | github.com/doobdev/doob'))
 
 @client.command()
 async def load(ctx, extension):
@@ -41,5 +42,5 @@ for filename in os.listdir('./cogs'):
 
 doob_logo = "https://cdn.discordapp.com/avatars/680606346952966177/ada47c5940b5cf8f7e12f61eefecc610.webp?size=1024"
 
-client.run("TOKEN")
+client.run("token")
 #hi mr jones lol
