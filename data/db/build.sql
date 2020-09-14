a820cc925d2108819e029b3274cca9304b54f2a9
mmatt625 <notmmatt@gmail.com>
2020-09-14 01:26:21 -0500

    0.0.1
    Started actually working on the bot, only a few things added so far tho
    Will work on it tomorrow.
    You can use these features now on the Beta which is avaliable on Patreon

---
+++ data/db/build.sql
@@ -0,0 +1,11 @@
+---CREATE TABLE IF NOT EXISTS guilds(
+---    GuildID integer PRIMARY KEY,
+---    Prefix text DEFAULT "doob/"
+---);
+
+CREATE TABLE IF NOT EXISTS exp (
+    UserID integer PRIMARY KEY,
+    XP integer DEFAULT 0,
+    Level integer DEFAULT 0,
+    XPLock text DEFAULT CURRENT_TIMESTAMP
+);
---
+++ lib/bot/__init__.py
@@ -0,0 +1,129 @@
+from asyncio import sleep
+from glob import glob
+
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from discord import Embed, File
+from discord.errors import HTTPException, Forbidden
+from discord.ext.commands import Bot as BotBase
+from discord.ext.commands import Context
+from discord.ext.commands import (CommandNotFound, BadArgument, MissingRequiredArgument, CommandOnCooldown)
+from discord.ext.commands import when_mentioned_or, command, has_permissions
+
+from ..db import db # pylint: disable=relative-beyond-top-level
+
+PREFIX = "doob/"
+OWNER_IDS = [308000668181069824]
+COGS = [path.split("\\")[-1][:-3] for path in glob("./lib/cogs/*.py")]
+IGNORE_EXCEPTIONS = (CommandNotFound, BadArgument)
+
+#def get_prefix(bot, message):
+#    prefix = db.field("SELECT Prefix FROM guilds WHERE GuildID = ?", message.guild.id)
+#    return when_mentioned_or(prefix)(bot, message)
+
+class Ready(object):
+    def __init__(self):
+        for cog in COGS:
+            setattr(self, cog, False)
+
+    def ready_up(self, cog):
+        setattr(self, cog, True)
+        print(f'{cog} cog ready')
+
+    def all_ready(self):
+        return all([getattr(self, cog) for cog in COGS])
+
+class Bot(BotBase):
+    def __init__(self):
+        self.ready = False
+        self.cogs_ready = Ready()
+
+        self.guild = None
+        self.scheduler = AsyncIOScheduler()
+
+        db.autosave(self.scheduler)
+
+        super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
+
+    def setup(self):
+        for cog in COGS:
+            self.load_extension(f"lib.cogs.{cog}")
+            print(f"{cog} cog loaded!")
+
+        print("Setup done!")
+
+    def run(self, version):
+        self.VERSION = version
+
+        print("Running setup!")
+        self.setup()
+
+        with open("./lib/bot/token.txt", "r", encoding="utf-8") as tf:
+            self.TOKEN = tf.read()
+
+        print("Starting up")
+        super().run(self.TOKEN, reconnect=True)
+
+    async def process_commands(self, message):
+        ctx = await self.get_context(message, cls=Context)
+
+        if ctx.command is not None and ctx.guild is not None:
+            if self.ready:
+
+                    await self.invoke(ctx)
+
+            else:
+                await ctx.send("Please wait, Doob is not online yet.")
+
+    async def on_connect(self):
+        print("Doob Connected")
+    
+    async def on_disconnect(self):
+        print("Doob Disconnected")
+
+    async def on_error(self, err, *args, **kwargs):
+        if err == "on_command_error":
+            await args[0].send("Something went wrong. :/")
+
+        raise err
+
+    async def on_command_error(self, ctx, exc):
+        if any([isinstance(exc, error) for error in IGNORE_EXCEPTIONS]):
+            pass
+        
+        elif isinstance(exc, MissingRequiredArgument):
+            await ctx.send("Required arguments missing.")
+
+        elif isinstance(exc, CommandOnCooldown):
+            await ctx.send(f'That command is on a {str(exc.cooldown.type).split(".")[-1]} cooldown! Try again in {exc.retry_after:,.2f} seconds.', delete_after = exc.retry_after)
+
+        elif hasattr(exc, "original"):
+            #if isinstance(exc.original, HTTPException):
+                #await ctx.send("Unable to send message.")
+
+            if isinstance(exc.original, Forbidden):
+                await ctx.send("Doob doesn't have permissions in this server to use that command.")
+
+            else:
+                raise exc.original
+
+        else:
+            raise exc
+
+
+    async def on_ready(self):
+        if not self.ready:
+            self.scheduler.start()
+            while not self.cogs_ready.all_ready():
+                await sleep(0.5)
+
+            self.ready = True
+            print("Doob Ready")
+
+        else:
+            print("Doob Reconnected")
+
+    async def on_message(self, message):
+        if not message.author.bot:
+            await self.process_commands(message)
+
+bot = Bot()
