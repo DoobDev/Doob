acd40da5a39f5627a7bff180f5c97db9c47ea2dd
mmatt625 <notmmatt@gmail.com>
2020-09-24 14:25:36 -0500

    work on this garbage later

--- lib/cogs/TopGG.py
+++ lib/cogs/TopGG.py
@@ -14,7 +14,7 @@ class TopGG(commands.Cog):
         self.bot = bot
         with open("./lib/bot/topgg.txt", "r", encoding="utf-8") as tf:
             self.token = tf.read() # set this to your DBL token
-        self.dblpy = dbl.DBLClient(self.bot, self.token, webhook_path='/dblwebhook', webhook_auth="bruhpaslol", webhook_port=5000)
+        self.dblpy = dbl.DBLClient(self.bot, self.token, webhook_path='/dblwebhook', webhook_auth=something, webhook_port=5000)
 
     # The decorator below will work only on discord.py 1.1.0+
     # In case your discord.py version is below that, you can use self.bot.loop.create_task(self.update_stats())
