0d5873fd6f0d176fba27d12c6b119f685b447e24
mmatt625 <notmmatt@gmail.com>
2020-09-24 13:41:28 -0500

    hm

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -61,13 +61,13 @@ class Bot(BotBase):
 
         print("Running setup!")
         self.setup()
-        print(os.getenv("TOKEN")) # bruh
+        #print(os.getenv("TOKEN")) # bruh
         # ^^ is printing "None"
-        #with open("./lib/bot/token.txt", "r", encoding="utf-8") as tf:
-            #self.TOKEN = tf.read()
+        with open("./lib/bot/token.txt", "r", encoding="utf-8") as tf:
+            self.TOKEN = tf.read()
         print("Starting up")
-        super().run(os.getenv("TOKEN"), reconnect=True)
-        #super().run(self.TOKEN, reconnect=True)
+        #super().run(os.getenv("TOKEN"), reconnect=True)
+        super().run(self.TOKEN, reconnect=True)
 
     async def process_commands(self, message):
         ctx = await self.get_context(message, cls=Context)
