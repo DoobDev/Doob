3db01c027a5dfc1cdea622812d97bddb7bba5fbb
mmatt625 <notmmatt@gmail.com>
2020-09-24 13:38:37 -0500

    hey spam pushes to server sorry :/

--- lib/cogs/voting.py
+++ lib/cogs/voting.py
@@ -15,13 +15,14 @@ class voting(commands.Cog):
         self.bot = bot
         self.token = os.getenv("TOPGG") # set this to your DBL token
         self.dblpy = dbl.DBLClient(self.bot, self.token, webhook_path='/dblwebhook', webhook_auth=os.getenv("WEBHOOK"), webhook_port=5000) #wtf is this how 2 webhook i dont kow
+    
     @commands.Cog.listener()
     async def on_dbl_vote(self, data):
         logger.info('Received an upvote')
         print(data)
 		#this is the webhook? can you send a test vote pls dont actually vote tho
 		#          yeeah i can send test webhook, but idk how 2 webhook  because it be like "webhook_path" and idk wtf that is pls HELP:|
-    @Cog.listener()
+    @commands.Cog.listener()
     async def on_ready(self):
         if not self.bot.ready:
             self.bot.cogs_ready.ready_up("voting")
