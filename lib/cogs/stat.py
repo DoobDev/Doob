47ef880e2a43928cf7f227b6d6ef956e2b71a86e
mmattbtw <matt@mmatt.net>
2020-10-27 00:48:34 -0500

    add servercount+stats back

---
+++ lib/cogs/stat.py
@@ -0,0 +1,27 @@
+from discord.ext import commands
+
+import statcord
+
+
+class stat(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        with open("./lib/bot/statcord.txt", "r", encoding="utf-8") as tf:
+            self.key = tf.read()
+
+        self.api = statcord.Client(self.bot, self.key)
+        self.api.start_loop()
+
+
+    @commands.Cog.listener()
+    async def on_command(self,ctx):
+        self.api.command_run(ctx)
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        if not self.bot.ready:
+            self.bot.cogs_ready.ready_up("stat")
+
+def setup(bot):
+    bot.add_cog(stat(bot))
