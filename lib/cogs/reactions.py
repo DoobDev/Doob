41994328e3c6027b224df6c77fac300c694487ca
mmatt <notmmatt@gmail.com>
2020-11-13 21:57:56 -0600

    like 2 seconds of progress on #83

--- lib/cogs/reactions.py
+++ lib/cogs/reactions.py
@@ -47,6 +47,54 @@ class Reactions(Cog):
             else:
                 await message.remove_reaction(payload.emoji, payload.member)
 
+    # async def on_starboard_remove(self, payload):
+    #     if payload.emoji.name == "⭐":
+    #         guild = self.bot.get_guild(payload.guild_id)
+    #         starboardchannel = await self.bot.fetch_channel(db.field("SELECT StarBoardChannel from guilds WHERE GuildID = ?", guild.id))
+    #         message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
+
+    #         if not message.author.bot and payload.member.id != message.author.id:
+    #             msg_id, stars = db.record("SELECT StarMessageID, Stars from starboard WHERE (GuildID, MessageID) = (?, ?)", guild.id, message.id) or (None, 0)
+    #             embed = Embed(title=f"⭐ x{stars-1}", colour=message.author.colour, timestamp=datetime.utcnow())
+    #             embed.set_thumbnail(url=message.author.avatar_url)
+    #             embed.set_footer(text=f"⭐ x{stars-1}")
+    #             #pip ( gotta get that proper indentation B) )
+    #             fields = [("Author", message.author.mention, False),
+    #                     ("Content", message.content or "Image", False),
+    #                     ("Jump To Link", f"[Jump](https://discord.com/channels/{message.guild.id}/{message.channel.id}/{message.id})", False)]
+
+    #             for name, value, inline in fields:
+    #                 embed.add_field(name=name, value=value, inline=inline)
+
+    #             if len(message.attachments):
+    #                 embed.set_image(url=message.attachments[0].url)
+
+    #             if not stars:
+    #                 star_message = await starboardchannel.send(embed=embed)
+    #                 db.execute("INSERT INTO starboard (MessageID, StarMessageID, GuildID) VALUES (?, ?, ?)", message.id, star_message.id, message.guild.id)
+    #                 db.commit()
+
+    #             else:
+    #                 star_message = await starboardchannel.fetch_message(msg_id)
+    #                 await star_message.edit(embed=embed)
+    #                 db.execute("UPDATE starboard SET Stars = Stars - 1 WHERE (GuildID, MessageID) = (?, ?)", message.guild.id, message.id)
+    #                 db.commit()
+
+    #         else:
+    #             await message.remove_reaction(payload.emoji, payload.member)
+
+    # @Cog.listener()
+    # async def on_reaction_remove(self, payload):
+    #     await self.on_starboard_remove(payload)
+
+    # @Cog.listener()
+    # async def on_reaction_clear_emoji(self, reaction):
+    #     await self.on_starboard_remove(payload=reaction)
+
+    # @Cog.listener()
+    # async def on_reaction_clear(self, reaction):
+    #     await self.on_starboard_remove(payload=reaction)
+        
     @Cog.listener()
     async def on_ready(self):
         if not self.bot.ready:
