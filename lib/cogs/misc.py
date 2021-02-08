bd63e200c33342352de899c6acdb02e44e20fda4
matt <matt@mmatt.net>
2021-02-07 21:30:01 -0600

    🔧Fixed spelling on overlay command.

--- lib/cogs/misc.py
+++ lib/cogs/misc.py
@@ -261,7 +261,7 @@ class Misc(Cog):
         streamkit_url = f"https://streamkit.discord.com/overlay/voice/{ctx.guild.id}/{ctx.author.voice.channel.id}?icon=true&online=true&logo=white&text_color=%23ffffff&text_size=14&text_outline_color=%23000000&text_outline_size=0&text_shadow_color=%23000000&text_shadow_size=0&bg_color=%231e2124&bg_opacity=0.95&bg_shadow_color=%23000000&bg_shadow_size=0&invite_code=&limit_speaking=false&small_avatars=false&hide_names=false&fade_chat=0"
 
         embed = Embed(
-            title="Straemkit Overlay:",
+            title="Streamkit Overlay:",
             description=streamkit_url,
             colour=ctx.author.colour,
             timestamp=datetime.utcnow(),
