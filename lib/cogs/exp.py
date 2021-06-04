2e446ee3770c7c5c6ec5899a243952f373c2ba53
Timothy Pidashev <pidashev.tim@gmail.com>
2021-06-04 10:29:00 -0700

    Update exp.py

--- lib/cogs/exp.py
+++ lib/cogs/exp.py
@@ -7,6 +7,7 @@ from discord.ext.commands import command, has_permissions
 from discord.ext.menus import MenuPages, ListPageSource
 from lib.bot import bot  # pylint: disable=no-name-in-module, import-error
 from ..db import db  # pylint: disable=relative-beyond-top-level
+from PIL import Image, ImageDraw, ImageFont
 
 
 class Menu(ListPageSource):
