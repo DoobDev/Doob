e17dce23db035d32de412ee74c6937ced006a233
AbelCoder <abelstef591@gmail.com>
2021-06-04 02:51:56 +0000

    add mee6 like rank cards

--- lib/cogs/exp.py
+++ lib/cogs/exp.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 from random import randint
 from typing import Optional
-
+import os
 from discord import Member, Embed
 from discord.ext.commands import Cog
 from discord.ext.commands import command, has_permissions
@@ -155,32 +155,105 @@ class Exp(Cog):
                     delete_after=10,
                 )
 
-    @command(name="level", aliases=["rank", "lvl"], brief="Shows your level, and rank.")
-    async def display_level(self, ctx, target: Optional[Member]):
-        """Shows your Global+Server Doob level, rank and XP!"""
-        target = target or ctx.author
+    @commands.command()
+    async def rank(self, ctx, target: Optional[Member]):
+        await log.cog_command(self, ctx)
+        target = target or context.author
 
-        ids = db.column("SELECT UserID FROM users ORDER BY XP DESC")
-        # ids_g = db.column("SELECT UserID FROM users ORDER BY XP DESC WHERE GuildID = ?", ctx.guild.id)
-        xp, lvl = db.record(
-            "SELECT XP, Level FROM users WHERE UserID = ?", target.id
-        ) or (None, None)
-        xp_g, lvl_g = (
-            db.record(
-                "SELECT XP, Level FROM guildexp WHERE (UserID, GuildID) = (?, ?)",
-                target.id,
-                ctx.guild.id,
-            )
-            or (None, None)
+        exp, level = db.record(f"SELECT XP, Level FROM users WHERE (guildID, UserID) = (?, ?)",
+            target.guild.id,
+            target.id
         )
+        ids = db.column(f"SELECT UserID FROM users WHERE GuildID = {target.guild.id} ORDER BY XP DESC")
 
-        if lvl is not None:
-            await ctx.reply(
-                f"`Global Rank:`\n{target.display_name} is level {lvl:,} with {xp:,} xp and is rank {ids.index(target.id)+1} of {len(ids):,} users globally.\n`Server Rank:`\n{target.display_name} is server level {lvl_g:,} with {xp_g:,} server xp."
-            )
+
+        if exp or level is not None:
+            async with ctx.typing():
+                await asyncio.sleep(1)
+
+                rank = f"{ids.index(target.id)+1}"
+                final_xp = 1000#this is where the formula i opened the issue would make a lot of sense. In my testing i was not able to get that to work though. 
+                xp = exp
+                user_name = str(target.name)
+                discriminator = f"#{target.discriminator}"
+                Level = int(((exp) // 42) ** 0.55)
+
+                background = Image.new("RGB", (1000, 240))
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(str(target.avatar_url)) as response:
+                        image = await response.read()
+                        icon = Image.open(BytesIO(image)).convert("RGBA").resize((200, 200))
+                        bigsize = (icon.size[0] * 3, icon.size[1] * 3)
+                        mask = Image.new("L", bigsize, 0)
+                        draw = ImageDraw.Draw(mask)
+                        draw.ellipse((0, 0) + bigsize, 255)
+                        draw.ellipse((140 * 3, 140 * 3, 189 * 3, 189 * 3), 0)
+                        mask = mask.resize(icon.size, Image.ANTIALIAS)
+                        icon.putalpha(mask)
+                        background.paste(icon, (20, 20), mask=icon)
+                        draw = ImageDraw.Draw(background, "RGB")
+                        draw.ellipse((162, 162, 206, 206), fill="#43B581")
+                        big_font = ImageFont.FreeTypeFont("./lib/fonts/ABeeZee-Regular.otf", 60)
+                        medium_font = ImageFont.FreeTypeFont("./lib/fonts/ABeeZee-Regular.otf", 40)
+                        small_font = ImageFont.FreeTypeFont("./lib/fonts/ABeeZee-Regular.otf", 30)
+                        text_size = draw.textsize(str(level), font=big_font)
+                        offset_x = 1000 - 15 - text_size[0]
+                        offset_y = 10
+                        draw.text((offset_x, offset_y), str(level), font=big_font, fill="#11ebf2")
+                        text_size = draw.textsize("LEVEL", font=small_font)
+                        offset_x -= text_size[0] + 5
+                        draw.text((offset_x, offset_y + 27), "LEVEL", font=small_font, fill="#11ebf2")
+                        text_size = draw.textsize(f"#{rank}", font=big_font)
+                        offset_x -= text_size[0] + 15
+                        draw.text((offset_x, offset_y), f"#{rank}", font=big_font, fill="#fff")
+                        text_size = draw.textsize("RANK", font=small_font)
+                        offset_x -= text_size[0] + 5
+                        draw.text((offset_x, offset_y + 27), "RANK", font=small_font, fill="#fff")
+                        bar_offset_x = 320
+                        bar_offset_y = 160
+                        bar_offset_x_1 = 950
+                        bar_offset_y_1 = 200
+                        circle_size = bar_offset_y_1 - bar_offset_y
+                        draw.rectangle((bar_offset_x, bar_offset_y, bar_offset_x_1, bar_offset_y_1), fill="#727175")
+                        draw.ellipse(
+                            (bar_offset_x - circle_size // 2, bar_offset_y, bar_offset_x + circle_size // 2, bar_offset_y_1), fill="#727175"
+                        )
+                        draw.ellipse(
+                            (bar_offset_x_1 - circle_size // 2, bar_offset_y, bar_offset_x_1 + circle_size // 2, bar_offset_y_1), fill="#727175"
+                        )
+                        bar_length = bar_offset_x_1 - bar_offset_x
+                        progress = (final_xp - xp) * 100 / final_xp
+                        progress = 100 - progress
+                        progress_bar_length = round(bar_length * progress / 100)
+                        bar_offset_x_1 = bar_offset_x + progress_bar_length
+                        draw.rectangle((bar_offset_x, bar_offset_y, bar_offset_x_1, bar_offset_y_1), fill="#11ebf2")
+                        draw.ellipse(
+                            (bar_offset_x - circle_size // 2, bar_offset_y, bar_offset_x + circle_size // 2, bar_offset_y_1), fill="#11ebf2"
+                        )
+                        draw.ellipse(
+                            (bar_offset_x_1 - circle_size // 2, bar_offset_y, bar_offset_x_1 + circle_size // 2, bar_offset_y_1), fill="#11ebf2"
+                        )
+                        text_size = draw.textsize(f"/ {final_xp} XP", font=small_font)
+                        offset_x = 950 - text_size[0]
+                        offset_y = bar_offset_y - text_size[1] - 10
+                        draw.text((offset_x, offset_y), f"/ {final_xp:,} XP", font=small_font, fill="#727175")
+                        text_size = draw.textsize(f"{xp:,}", font=small_font)
+                        offset_x -= text_size[0] + 8
+                        draw.text((offset_x, offset_y), f"{xp:,}", font=small_font, fill="#fff")
+                        text_size = draw.textsize(user_name, font=medium_font)
+                        offset_x = bar_offset_x
+                        offset_y = bar_offset_y - text_size[1] - 5
+                        draw.text((offset_x, offset_y), user_name, font=medium_font, fill="#fff")
+                        offset_x += text_size[0] + 5
+                        offset_y += 10
+                        draw.text((offset_x, offset_y), discriminator, font=small_font, fill="#727175")
+                        background.show()
+                        background.save("./images/imgswap.png")
+                        ffile = discord.File("./images/imgswap.png")
+                        await context.channel.send(file=ffile)
 
         else:
-            ctx.reply("That member is not in the XP Database.")
+            await context.reply("You are not in the database :(\nDont worry though, you were just added! Try running the command again.", mention_author=False)
 
     @command(
         name="levelmessages",
