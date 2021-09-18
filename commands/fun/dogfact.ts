05dd2a77db7767d1921f6d8f4053cee1cdbbdd3b
mmatt <matt@mmatt.net>
2021-09-18 00:41:47 -0500

    feat(commands): Added dogfact command.

---
+++ commands/fun/dogfact.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { MessageEmbed } from 'discord.js';
+import { ICommand } from 'wokcommands';
+import { getDoobColor } from '../../utils/colors';
+import { getEmote } from '../../utils/emotes';
+
+export default {
+    name: 'dogfact',
+    description: 'Get a random dog fact',
+    usage: 'dogfact',
+    category: 'Fun',
+
+    slash: true,
+    testOnly: true,
+
+    callback: async ({}) => {
+        try {
+            let data = await axios.get('https://some-random-api.ml/facts/dog');
+            const embed = new MessageEmbed().setDescription(`${getEmote('transparent>')} ${data.data.fact}`).setColor(getDoobColor('DOOB'));
+            return embed;
+        } catch (error) {
+            return `An error occurred: ${error}`;
+        }
+    },
+} as ICommand;
