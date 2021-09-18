47a9667afc20513eda89689fef0a00646f2065d5
mmatt <matt@mmatt.net>
2021-09-18 00:41:55 -0500

    feat(commands): Added vote command

---
+++ commands/meta/vote.ts
@@ -0,0 +1,22 @@
+import { MessageEmbed } from 'discord.js';
+import { ICommand } from 'wokcommands';
+import { getDoobColor } from '../../utils/colors';
+import { getEmote } from '../../utils/emotes';
+
+export default {
+    name: 'vote',
+    description: 'Vote for the bot on Top.gg',
+    usage: 'vote',
+    category: 'Meta',
+
+    slash: true,
+    testOnly: true,
+
+    callback: ({}) => {
+        const embed = new MessageEmbed()
+            .setDescription(`${getEmote('^')} Vote for Doob on Top.gg https://top.gg/bot/680606346952966177/vote ${getEmote('pinkStar')}`)
+            .setColor(getDoobColor('DOOB'));
+
+        return embed;
+    },
+} as ICommand;
