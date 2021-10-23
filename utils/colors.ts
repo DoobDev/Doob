21382008cb5de30b0b925bd34c3fd83986f37667
matt <matt@mmatt.net>
2021-10-23 00:44:06 -0500

    feat(utils): changed color codes

--- utils/colors.ts
+++ utils/colors.ts
@@ -1,8 +1,8 @@
 export function getDoobColor(color: string) {
     let colorName = color.toUpperCase();
     let colorOptions: any = {
-        SUCCESS: 0x5ef059,
-        DANGER: 0xf05959,
+        SUCCESS: 0x8fff94,
+        DANGER: 0xff8f8f,
         WARNING: 0xf09d59,
         DOOB: 0xff99fa,
     };
