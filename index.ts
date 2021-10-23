8b3f4b4e5cb864e18e6aa6742005fc16c99156d0
matt <matt@mmatt.net>
2021-10-22 23:17:04 -0500

    refactor(core): Added more comments/documentation for index.ts

--- index.ts
+++ index.ts
@@ -12,6 +12,7 @@ const client = new DiscordJS.Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
 });
 const statcord = new Statcord.Client({
+    // Statistics client for statcord.com
     key: `${process.env.STATCORD_KEY}`,
     client,
     postCpuStatistics: true,
@@ -21,21 +22,21 @@ const statcord = new Statcord.Client({
 
 client.on('ready', () => {
     new WOKCommands(client, {
-        commandsDir: path.join(__dirname, 'commands'),
-        featuresDir: path.join(__dirname, 'features'),
-        typeScript: true,
-        testServers: ['702352937980133386', '815021537303986176'],
-        mongoUri: process.env.MONGO_URI,
-        disabledDefaultCommands: ['language'],
-        defaultLanguage: 'english',
-        ignoreBots: true,
-        delErrMsgCooldown: 5,
+        commandsDir: path.join(__dirname, 'commands'), // Directory where commands are stored
+        featuresDir: path.join(__dirname, 'features'), // Directory where features (event listeners pretty much) are stored
+        typeScript: true, // Enabled because Doob v3 uses Typescript
+        testServers: ['702352937980133386', '815021537303986176'], // The two Doob test servers  (70... is the Doob Dev server, 815... is a private test server)
+        mongoUri: process.env.MONGO_URI, // MongoDB connection string (Stored as MONGO_URI in .env)
+        disabledDefaultCommands: ['language'], // Disabled the language command in WOKCommands, I don't plan to support different languages until later.
+        defaultLanguage: 'english', // Default language for Doob is English.
+        ignoreBots: true, // WOKCommands ignores other Discord Bots, for no echoing of commands and such.
+        delErrMsgCooldown: 5, // After 5 seconds, the default WOKCommands error messages delete.
         dbOptions: { keepAlive: true },
-        botOwners: ['308000668181069824'],
+        botOwners: ['308000668181069824'], // For owner only commands (3080... is mmatt)
     })
-        .setDefaultPrefix('d!')
+        .setDefaultPrefix('d!') // Default prefix for legacy commands (which most are not used for the public) is d!
         // @ts-ignore
-        .setColor(getDoobColor('DOOB'))
+        .setColor(getDoobColor('DOOB')) // Default color for embeds like d!help
         .setCategorySettings([
             {
                 name: 'API',
