6aeea1e9062c6610b56f2e1d78a0bcd8025b39f3
matt <matt@mmatt.net>
2021-10-23 00:44:41 -0500

    feat(utils): added support for custom colors and images for generic embeds

--- utils/generic_embeds.ts
+++ utils/generic_embeds.ts
@@ -6,11 +6,16 @@ export function errorEmbed(text: string) {
     const embed = new MessageEmbed()
         .setDescription(`${getEmote('red!')} ERR: ${text}`)
         .setColor(getDoobColor('DANGER'))
-        .setFooter(`🛑 Make sure to join the support server to report this error.`);
+        .setFooter(`🛑 make sure to join the support server to report this error.`);
     return embed as MessageEmbed;
 }
 
-export function doobEmbed(text: string) {
-    const embed = new MessageEmbed().setDescription(text).setColor(getDoobColor('DOOB'));
+export function doobEmbed(text: string, color?: string | null, image?: string | null) {
+    const embed = new MessageEmbed().setDescription(text).setColor(color || getDoobColor('DOOB'));
+
+    if (image) {
+        embed.setThumbnail(image);
+    }
+
     return embed as MessageEmbed;
 }
