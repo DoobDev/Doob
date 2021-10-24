f41159254fd46c3607103388fe2df29f4722383a
matt <matt@mmatt.net>
2021-10-24 00:57:12 -0500

    feat(commands): added links command

--- COMMANDS_TO_KEEP.md
+++ COMMANDS_TO_KEEP.md
@@ -10,27 +10,27 @@
 5. user info command (info.py)
 6. server info command (info.py)
 7. lastfm commands (lastfm.py)
-8. link (links.py)
-9. support logging
+ <!-- 8. link (links.py) -->
+8. support logging
     <!-- 10. support (meta.py) -->
     <!-- 11. invite (meta.py) -->
     <!-- 17. ping (meta.py) -->
     <!-- 13. shutdown/restart (meta.py) // im just gonna use the github actions lol -->
     <!-- 19. patreon (meta.py) -->
-10. eval command (maybe) (meta.py)
-11. blacklist (meta.py)
-12. unblacklist (meta.py)
-13. afk (misc.py)
+9. eval command (maybe) (meta.py)
+10. blacklist (meta.py)
+11. unblacklist (meta.py)
+12. afk (misc.py)
 <!-- 24. per server prefixs -->
-14. poll (misc.py)
-15. endpoll (misc.py)
-16. giveaway (misc.py)
-17. endgiveaway (misc.py)
+13. poll (misc.py)
+14. endpoll (misc.py)
+15. giveaway (misc.py)
+16. endgiveaway (misc.py)
     <!-- 29. timebomb (misc.py) -->
     <!-- 21. vote (misc.py) -->
     <!-- 22. ownerprefix (misc.py) -->
     <!-- 21. overlay (misc.py) // scrapped -->
-18. emote (misc.py)
-19. most of everything in mod.py
-20. starboard
-21. twitch (move to v5 )
+17. emote (misc.py)
+18. most of everything in mod.py
+19. starboard
+20. twitch (move to v5 )
---
+++ commands/api/links.ts
@@ -0,0 +1,72 @@
+import { ICommand } from 'wokcommands';
+import { DiscordOption } from '../../utils/discordoptions';
+import axios from 'axios';
+import { doobEmbed, errorEmbed } from '../../utils/generic_embeds';
+import { getEmote } from '../../utils/emotes';
+import { TextChannel } from 'discord.js';
+
+export default {
+    name: 'shortenlink',
+    description: 'shorten a link using doob.link',
+    category: 'api',
+
+    options: [
+        {
+            name: 'link',
+            description: 'link to shorten',
+            required: true,
+            type: DiscordOption('STRING'),
+        },
+        {
+            name: 'vanity',
+            description: 'custom url ending',
+            required: false,
+            type: DiscordOption('STRING'),
+        },
+    ],
+
+    slash: true,
+    testOnly: true,
+    minArgs: 1,
+    expectedArgs: '<link> <vanity>',
+
+    callback: async ({ client, interaction }) => {
+        const link = interaction.options.getString('link');
+        const vanity = interaction.options.getString('vanity');
+        let resp;
+        try {
+            resp = (await axios.post(
+                'https://api.rebrandly.com/v1/links',
+                {
+                    destination: link,
+                    domain: { fullName: 'doob.link' },
+                    slashtag: vanity,
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        apikey: `${process.env.REBRANDLY_API_KEY}`,
+                        workspace: `${process.env.REBRANDLY_WORKSPACE}`,
+                    },
+                }
+            )) as any;
+        } catch (err) {
+            return errorEmbed(`short link api returned an error\n${getEmote('transparentSpace')}${getEmote('transparent>')} ${err}`);
+        }
+
+        if (resp.data.shortUrl) {
+            const logging_guild = client.guilds.cache.get('702352937980133386');
+            const logging_channel = logging_guild?.channels.cache.get('843657332083654726') as TextChannel;
+
+            logging_channel?.send({
+                content: `<@308000668181069824>`,
+                embeds: [
+                    doobEmbed(
+                        `new short link!\nfrom <@${interaction.member?.user.id}> (username: ${interaction.member?.user.username}#${interaction.member?.user.discriminator} // id: \`${interaction.member?.user.id}\`)\nshort link: ${resp.data.shortUrl}\nlong link: https://${link}`
+                    ),
+                ],
+            });
+            return doobEmbed(`boom! link shortened: [${getEmote('pinkLink')}](https://${resp.data.shortUrl}) https://${resp.data.shortUrl}`);
+        }
+    },
+} as ICommand;
