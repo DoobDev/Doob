a30fdf07804016bf7e98cc461c0dd2958c02ef0b
matt <matt@mmatt.net>
2021-10-24 01:32:36 -0500

    feat(commands): added base userinfo command

---
+++ commands/info/userinfo.ts
@@ -0,0 +1,47 @@
+import { ICommand } from 'wokcommands';
+import { DiscordOption } from '../../utils/discordoptions';
+import { MessageEmbed } from 'discord.js';
+import { errorEmbed } from '../../utils/generic_embeds';
+import { getDoobColor } from '../../utils/colors';
+
+export default {
+    name: 'userinfo',
+    description: 'get information about a user',
+    category: 'info',
+
+    options: [
+        {
+            name: 'user',
+            description: 'user to get info about',
+            required: false,
+            type: DiscordOption('USER'),
+        },
+    ],
+    minArgs: 0,
+
+    expectedArgs: '<user>',
+
+    slash: true,
+    testOnly: true,
+
+    callback: ({ interaction, user }) => {
+        let userToGet;
+        if (!interaction.options.getUser('user')) {
+            userToGet = user;
+        } else if (interaction.options.getUser('user')) {
+            userToGet = interaction.options.getUser('user');
+        } else {
+            return errorEmbed('user not found');
+        }
+
+        if (!userToGet) {
+            return errorEmbed('user not found');
+        }
+
+        return new MessageEmbed()
+            .setThumbnail(`${userToGet.avatarURL()}`)
+            .setTitle(`${userToGet.username}'s information.`)
+            .setDescription(`${userToGet.id}`)
+            .setColor(userToGet.accentColor ?? getDoobColor('DOOB'));
+    },
+} as ICommand;
