7dc09a4c9d10e192c42f274628c47b1200e114ac
mmatt <matt@mmatt.net>
2021-11-27 22:52:13 -0600

    feat(commands): added the base to topgg listening

--- index.ts
+++ index.ts
@@ -1,10 +1,27 @@
+import Topgg from '@top-gg/sdk';
 import DiscordJS, { Intents } from 'discord.js';
 import dotenv from 'dotenv';
+import express from 'express';
 import path from 'path';
 import Statcord from 'statcord.js';
+import { AutoPoster } from 'topgg-autoposter';
 import WOKCommands from 'wokcommands';
 import { getDoobColor } from './utils/colors';
+
 dotenv.config();
+
+const WEBHOOK_PORT = process.env.WEBHOOK_PORT || 4200;
+
+const webhook_server = express();
+const webhook = new Topgg.Webhook(`${process.env.WEBHOOK_AUTH}`);
+webhook_server.post(
+    '/dblwebhook',
+    webhook.listener((vote) => {
+        console.log(`${vote.user} has voted for doob on top.gg`);
+    })
+);
+
+webhook_server.listen(WEBHOOK_PORT);
 
 export const version = '3.0.0 [BETA]'; // The version the bot is running on, edit this on every release.
 
@@ -12,6 +29,13 @@ const client = new DiscordJS.Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
     // Enables intents for `Guilds`, `Guild Messages` and `Reactions from Guild Message`
 });
+
+const topgg_ap = AutoPoster(`${process.env.TOPGG_KEY}`, client);
+
+topgg_ap.on('posted', () => {
+    console.log('stats have been posted to top.gg');
+});
+
 const statcord = new Statcord.Client({
     // Statistics client for statcord.com
     key: `${process.env.STATCORD_KEY}`,
