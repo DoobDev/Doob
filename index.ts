3e395cff7b9e264cf57953746650503efcac82cc
matt <30363562+mmattbtw@users.noreply.github.com>
2021-12-04 00:28:15 -0600

    feat(core): added top.gg features (#329)
    - currently doesn't have any function

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
+import { Webhook } from '@top-gg/sdk';
 import DiscordJS, { Intents } from 'discord.js';
 import dotenv from 'dotenv';
+import express from 'express';
 import path from 'path';
 import Statcord from 'statcord.js';
+import { AutoPoster } from 'topgg-autoposter';
 import WOKCommands from 'wokcommands';
 import { getDoobColor } from './utils/colors';
+
 dotenv.config();
+
+const WEBHOOK_PORT = process.env.WEBHOOK_PORT || 80;
+
+const webhook_server = express();
 
 export const version = '3.0.0 [BETA]'; // The version the bot is running on, edit this on every release.
 
@@ -12,6 +20,14 @@ const client = new DiscordJS.Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
     // Enables intents for `Guilds`, `Guild Messages` and `Reactions from Guild Message`
 });
+
+// Top.gg Auto post stats
+const topgg_ap = AutoPoster(`${process.env.TOPGG_KEY}`, client);
+
+topgg_ap.on('posted', () => {
+    console.log('stats have been posted to top.gg');
+});
+
 const statcord = new Statcord.Client({
     // Statistics client for statcord.com
     key: `${process.env.STATCORD_KEY}`,
@@ -82,5 +98,21 @@ statcord.on('autopost-start', () => {
     // Emitted when statcord autopost starts
     console.log('Started Statcord autopost');
 });
+
+// Top.gg Webhook integration
+const webhook = new Webhook(`${process.env.WEBHOOK_AUTH}`);
+webhook_server.post(
+    '/dblwebhook',
+    webhook.listener((vote) => {
+        console.log(vote.user);
+    })
+);
+
+webhook_server.get('/', (req, res) => {
+    res.sendStatus(200);
+});
+
+webhook_server.listen(WEBHOOK_PORT);
+console.log(`Webhook server listening on port ${WEBHOOK_PORT}`);
 
 client.login(process.env.TOKEN);
